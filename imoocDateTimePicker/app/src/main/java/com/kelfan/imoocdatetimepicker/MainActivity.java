6a2e22dc402b8738df7de2d03e6e66a10d96a988
zcfan <zchaofan08@gmail.com>
2017-12-03 11:26:42 +1100

    Change structure + imoocDateTimePicker/

---
+++ imoocDateTimePicker/app/src/main/java/com/kelfan/imoocdatetimepicker/MainActivity.java
@@ -0,0 +1,59 @@
+package com.kelfan.imoocdatetimepicker;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //获取日历的一个对象
+        calendar = Calendar.getInstance();
+        // 获取年月日时分秒的信息
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+        setTitle(year+"-"+month+"-"+day+"-"+hour+"-"+minute);
+
+        datePicker = findViewById(R.id.datePicker);
+        timePicker = findViewById(R.id.timePicker);
+
+        //datepicker初始化
+        datePicker.init(year, calendar.get(Calendar.MINUTE), day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                setTitle(year+"-"+(monthOfYear+1)+"-"+dayOfMonth);
+            }
+        });
+
+        //Timepicker初始化
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hourOfDay, int minute) {
+                setTitle(hourOfDay+":"+minute);
+            }
+        });
+
+        new DatePickerDialog()
+    }
+}
