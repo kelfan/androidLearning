43bfbf0bd854eead8f831dfce041759b4e7c4db3
zcfan <zchaofan08@gmail.com>
2018-01-01 11:04:55 +1100

    editText V1

---
+++ MyProject/fanTextEditor/README.md
@@ -0,0 +1,3 @@
+# fanTextEditor v1
+This is a single text editor to save and sort long text.
+![](assets/README-82597b55.png)
---
+++ MyProject/fanTextEditor/app/src/main/java/com/kelfan/fantexteditor/Constant.java
@@ -0,0 +1,10 @@
+package com.kelfan.fantexteditor;
+
+/**
+ * Created by Administrator on 1/01/2018.
+ */
+
+public class Constant {
+    public static final String PATH = "05fanNotes";
+    public static final String FILENAME = "2018TodList.py";
+}
