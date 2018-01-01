6eec1b8681eded2853f10b54feb42a7d8eebeb84
zcfan <zchaofan08@gmail.com>
2018-01-01 23:23:39 +1100

    fanTextEditor v2

---
+++ MyProject/fanTextEditorV2/app/src/main/java/com/kelfan/fantexteditorv2/Constant.java
@@ -0,0 +1,10 @@
+package com.kelfan.fantexteditorv2;
+
+/**
+ * Created by Administrator on 1/01/2018.
+ */
+
+public class Constant {
+    public static final String PATH = "05fanNotes";
+    public static final String FILENAME = "2018TodList.py";
+}
