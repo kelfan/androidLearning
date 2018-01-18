3d8f0f8dc06700a59dc3894d2741d5e27df03dfd
zcfan <zchaofan08@gmail.com>
2018-01-18 21:09:35 +1100

    Tditor add text change for repeat first letter

--- MyProject/Tditor/app/src/main/java/Util/StringStyleWorker.java
+++ MyProject/Tditor/app/src/main/java/Util/StringStyleWorker.java
@@ -18,8 +18,8 @@ public class StringStyleWorker {
     public static SpannableString setTextColor(String inStr, int color, String s2) {
         SpannableString s = new SpannableString(inStr);
         s.setSpan(new ForegroundColorSpan(color),
-                0, inStr.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-        s.setSpan(new BackgroundColorSpan(color),0,1, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+                0, inStr.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        s.setSpan(new BackgroundColorSpan(color),0,1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return s;
     }
 
@@ -28,8 +28,8 @@ public class StringStyleWorker {
         int pos1 = inStr.toLowerCase().indexOf(range.toLowerCase().charAt(0));
         int pos2 = inStr.toLowerCase().indexOf(range.toLowerCase().charAt(1)) + 1;
         s.setSpan(new ForegroundColorSpan(ColorWorker.getComplimentColor(color)),
-                pos1, pos2, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-        s.setSpan(new BackgroundColorSpan(color),pos1,pos2, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+                pos1, pos2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        s.setSpan(new BackgroundColorSpan(color),pos1,pos2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         return s;
     }
 
@@ -39,8 +39,8 @@ public class StringStyleWorker {
         for (int i=0; i<sList.length-1;i++) {
             SpannableString s = new SpannableString(sList[i]+delimiter);
             s.setSpan(new ForegroundColorSpan(ColorWorker.getComplimentColor(color)),
-                    0, sList[i].length()+1, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-            s.setSpan(new BackgroundColorSpan(color),0,sList[i].length()+1, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+                    0, sList[i].length()+1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            s.setSpan(new BackgroundColorSpan(color),0,sList[i].length()+1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             color = ColorWorker.getComplimentColor(color);
             result = TextUtils.concat(result, s);
         }
