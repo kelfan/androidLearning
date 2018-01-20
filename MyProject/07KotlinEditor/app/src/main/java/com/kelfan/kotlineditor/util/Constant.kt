0b8758f14ed6199810723c35160e030a21314de1
zcfan <zchaofan08@gmail.com>
2018-01-21 00:04:59 +1100

    fileworker

---
+++ MyProject/07KotlinEditor/app/src/main/java/com/kelfan/kotlineditor/util/Constant.kt
@@ -0,0 +1,18 @@
+package com.kelfan.kotlineditor.util
+
+/**
+ * Created by Administrator on 20/01/2018.
+ */
+class Constant{
+    companion object{
+        const val STORAGE_DATA = 0
+        const val STORAGE_SD = 1
+        const val STORAGE_SECOND_SD = 2
+
+        const val FILE_APPEND = 0
+        const val FILE_WRITE = 1
+
+        const val SUCCESS = 1
+        const val FAIL = 0
+    }
+}
