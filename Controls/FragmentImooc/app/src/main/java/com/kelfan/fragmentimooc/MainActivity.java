af56beffffc58edc24d2ff460b026fc7b5b6fc43
zcfan <zchaofan08@gmail.com>
2018-03-05 23:09:20 +1100

    fragment 动态加载

---
+++ Controls/FragmentImooc/app/src/main/java/com/kelfan/fragmentimooc/MainActivity.java
@@ -0,0 +1,58 @@
+package com.kelfan.fragmentimooc;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_static:
+                    mTextMessage.setText("静态加载");
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                    return true;
+                case R.id.navigation_dynamic:
+                    mTextMessage.setText("动态加载");
+                    DynamicFragment dynamicFragment = new DynamicFragment();
+                    FragmentManager fragmentManager = getFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    // 第一个参数是 装fragment的元素Id, 第二个参数是 fragment对象
+                    fragmentTransaction.add(R.id.frame, dynamicFragment);
+                    // 可以让fragment可以返回到上一个
+                    fragmentTransaction.addToBackStack(null);
+                    // 添加完后就是提交
+                    fragmentTransaction.commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
