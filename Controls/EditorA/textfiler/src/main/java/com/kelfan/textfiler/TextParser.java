14760d86f0a443a7acb01f2e528d6e450a531877
zcfan <zchaofan08@gmail.com>
2018-04-22 19:41:54 +1000

    TextParser set get xml content

--- Controls/EditorA/textfiler/src/main/java/com/kelfan/textfiler/TextParser.java
+++ Controls/EditorA/textfiler/src/main/java/com/kelfan/textfiler/TextParser.java
@@ -5,25 +5,33 @@ import com.kelfan.utillibrary.StringWorker;
 
 public class TextParser {
 
-    private String[] delimiter = {"\n", ":"};
-    private String[] levels = {"!", "\t"};
+    private String[] delimiter = {"\n"};
+    private String[] levels = {};
 
-    public static String getFileText(String filepath){
+    public static String getFileText(String filepath) {
         return FileWorker.readSmallTxtFile(filepath);
     }
 
-    public static String getXmlContent(String textIn, String indicator){
-        String pre = String.format("<%s>\n", indicator);
-        String pos = String.format("\n</%s>\n", indicator);
-        if (textIn.contains(pre)){
+    public static String getPreFormat(String indicator) {
+        return String.format("<%s>\n", indicator);
+    }
+
+    public static String getPosFormat(String indicator) {
+        return String.format("\n</%s>\n", indicator);
+    }
+
+    public static String getXmlContent(String textIn, String indicator) {
+        String pre = getPreFormat(indicator);
+        String pos = getPosFormat(indicator);
+        if (textIn.contains(pre)) {
             return StringWorker.getBetween(textIn, pre, pos);
         }
         return "";
     }
 
-    public static String setXmlContent(String textIn, String indicator){
-        String pre = String.format("<%s>\n", indicator);
-        String pos = String.format("\n</%s>\n", indicator);
+    public static String setXmlContent(String textIn, String indicator) {
+        String pre = getPreFormat(indicator);
+        String pos = getPosFormat(indicator);
         return pre.concat(textIn).concat(pos);
 
     }
